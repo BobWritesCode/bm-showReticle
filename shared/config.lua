ccd4f3f3b7eb10a823b271e39e333ecacd23b06b
BobWritesCode <warwick_hart@hotmail.com>
2024-03-19 23:32:43 +0000

    Add weapon config

---
+++ shared/config.lua
@@ -0,0 +1,14 @@
+Config = {
+
+  -- Toggle debug mode on and off
+  Debug = false,
+
+  -- Add an weapon to the list below you want reticle/dot to appear when aiming.
+  Weapons = {
+    205991906, --[[WEAPON_HEAVYSNIPER]]
+    177293209, --[[WEAPON_HEAVYSNIPER_MK2]]
+    3342088282, --[[WEAPON_MARKSMANRIFLE]]
+    785463520, --[[WEAPON_MARKSMANRIFLE_MK2]]
+    100416529 --[[WEAPON_SNIPERRIFLE]]
+  }
+}
