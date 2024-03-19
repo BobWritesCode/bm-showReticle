85d9b396799472b9f2ea2279b738a465bb874a78
BobWritesCode <warwick_hart@hotmail.com>
2024-03-19 23:33:40 +0000

    Add func loop to check for weapon in config

--- client/main.lua
+++ client/main.lua
@@ -19,6 +19,16 @@ RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
   isPlayerLoaded = false
 end)
 
+-- Return true/false depending if weapon player is holding aiming with is in Config.Weapons
+function CheckWeapon()
+  for _, value in pairs(Config.Weapons) do
+    if GetSelectedPedWeapon(PlayerPedId()) == value then
+      return true
+    end
+  end
+  return false
+end
+
 CreateThread(function()
   while true do
     while not isPlayerLoaded do
@@ -26,9 +36,12 @@ CreateThread(function()
     end
     while isPlayerLoaded do
       Wait(1)
-      -- 177293209 = WEAPON_HEAVYSNIPER_MK2
-      if IsPlayerFreeAiming(PlayerId()) and GetSelectedPedWeapon(PlayerPedId()) == 177293209 then
-        ShowHudComponentThisFrame(14)
+      if IsPlayerFreeAiming(PlayerId()) then
+        if CheckWeapon() then
+          ShowHudComponentThisFrame(14)
+        else
+          HideHudComponentThisFrame(14)
+        end
       end
     end
     Wait(1)
